db4be00f549e4419bdb94be4976c33dd613e5b66
Aryan <aryanrama26@gmail.com>
2024-02-13 05:08:32 +0100

    Registered Music

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import discord
+from cogs.music import Music
 from dotenv import load_dotenv
+import discord.ext.commands as commands
+
 
 load_dotenv()
 token = os.getenv("TOKEN")
@@ -8,11 +11,11 @@ token = os.getenv("TOKEN")
 intents = discord.Intents.all()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Logged on as {client.user}')
 
-
+client.add_cog(Music(client))
 client.run(token)
