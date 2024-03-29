6419b12ee4f2882885221bbd19432d1e192a3417
Aryan Rama <aryanrama26@gmail.com>
2024-03-29 12:43:17 +0100

    Added leave command and updated join

--- cogs/music.py
+++ cogs/music.py
@@ -4,23 +4,33 @@ from discord.ext import commands
 
 class Music(commands.Cog):
     def __init__(self, client):
-        self.client = client
+        self.client: discord.Client = client
     
     @commands.slash_command()
-    async def join(self, ctx):
-        """Joins a voice channel"""
+    async def join(self, ctx: discord.ApplicationContext):
+        """Makes the bot join a voice channel."""
+        if ctx.voice_client:
+            if len(ctx.author.voice.channel.members) > 1:
+                await ctx.respond("I'm already in a voice channel.")
+                return
         if ctx.author.voice != None:
             await ctx.author.voice.channel.connect()
             await ctx.respond("Joining...")
         else:
-            await ctx.respond("You must be in a voice channel")
-        
-    # @commands.slash_command()
-    # @commands.has_guild_permissions(administrator=True)
-    # async def test2(self, ctx=discord.ApplicationContext):
-    #     await ctx.respond("Okay")
-    #     print(self.client.voice_clients[0].guild)
+            await ctx.respond("You must be in a voice channel to use that command.")
     
+    @commands.slash_command()
+    async def leave(self, ctx: discord.ApplicationContext):
+        """Makes the bot leave a voice channel."""
+        if not ctx.guild.voice_client:
+            await ctx.respond("I am not in a voice channel.")
+        elif not ctx.author.voice:
+            await ctx.respond("You must be in a voice channel to use that command.")
+        elif ctx.author.voice.channel == ctx.guild.voice_client.channel:
+            await ctx.respond("Leaving...")
+            ctx.guild.voice_client.disconnect()
+        else:
+            await ctx.respond("You must be in the same voice channel to use this command.")
             
     @commands.Cog.listener(name="on_voice_state_update")
     async def LeaveAfter5(self=commands.Bot, member=discord.Member, before=discord.VoiceState, after=discord.VoiceState):
